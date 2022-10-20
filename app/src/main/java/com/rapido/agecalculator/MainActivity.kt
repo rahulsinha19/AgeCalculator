c46d400d43172daa8f7b5e337b6e3b9f12a7334a
Rahul Sinha <rahul.sinha2k@gmail.com>
2022-10-20 20:22:53 +0530

    Setting up date picker dialog for choosing date of birth

--- app/src/main/java/com/rapido/agecalculator/MainActivity.kt
+++ app/src/main/java/com/rapido/agecalculator/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.rapido.agecalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.Calendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnDatePicker : Button = findViewById(R.id.btn_date_picker)
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker() {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this,
+            DatePickerDialog.OnDateSetListener{ view, year, month, dayOfMonth ->
+
+            },
+            year,
+            month,
+            day
+            ).show()
     }
 }
